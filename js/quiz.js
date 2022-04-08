d832f0ea2fffe0418c5ce992edaf66847cf1bab2
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-08 12:37:21 +0300

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,14 @@
 // 1. Create a multidimensional array to hold quiz questions and answers
+const questions = [
+  ['How many planets are in the Solar System?', '8'],
+  ['How many continents are there?', '7'],
+  ['How many legs does an insect have?', '6'],
+  ['What year was javascript created?', '1995'],
 
+];
 
 // 2. Store the number of questions answered correctly
-
+let correctAnswer = 0;
 
 /* 
   3. Use a loop to cycle through each question
@@ -11,6 +17,20 @@
       - If the response matches the answer, the number of correctly
         answered questions increments by 1
 */
+for ( let i = 0; i < questions.length; i++) {
+  let question = questions[i][0];
+  let answer = questions[i][1];
+  let response = prompt(question);
 
+  if ( response === answer ) {
+    correctAnswer++;
+  }
+}
 
 // 4. Display the number of correct answers to the user
+
+let html = `
+  <h1>You got ${correctAnswer} question(s) correct</h1>
+`;
+
+document.querySelector('main').innerHTML = html;
